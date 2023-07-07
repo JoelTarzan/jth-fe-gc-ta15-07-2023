55d3f5553fd7f9f3058dd7b4c73eb4948f08c77c
Joel Tarzán <45524750+JoelTarzan@users.noreply.github.com>
2023-07-07 17:58:41 +0200

    Componentes añadidos, sistema de rutas creado, y navbar acabado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'jth-fe-gc-ta15-07-2023';
+export class AppComponent implements OnInit {
+  
+  inAuthPage: boolean = false;
+  currentPage: string = '';
+
+  constructor(private router: Router) {
+  }
+
+  ngOnInit(): void {
+
+    this.router.events.subscribe(event => {
+
+      if (event instanceof NavigationEnd) {
+        this.inAuthPage = (event.url === '/login' || event.url === '/register');
+        this.currentPage = event.url;
+      }
+      
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { LoginComponent } from './auth/login/login.component';
+import { HomeComponent } from './main/home/home.component';
+import { CharactersComponent } from './main/characters/characters.component';
+import { AboutComponent } from './main/about/about.component';
+import { DetailsComponent } from './main/details/details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    HomeComponent,
+    CharactersComponent,
+    AboutComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule,
